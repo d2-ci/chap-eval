f64cacf04a8d6f4fffc11faf4c8a61470efd0943
GitHub <noreply@github.com>
2025-05-22 13:57:56 +0000

    8ed9cf5 feat: add new evaluations page and table (#40)

---
+++ src/components/DynamicDemo.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import BackTestRunner from '../components/BackTestRunner'
+
+const DynamicDemo: React.FC = () => {
+    return <BackTestRunner />
+}
+export default DynamicDemo
